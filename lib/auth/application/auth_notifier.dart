b41641494477fb171dc50fba1c8571f755d8dc7f
titis <titis.rizki@gmail.com>
2022-04-28 05:28:47 +0700

    implement signin/signout notifier

--- lib/auth/application/auth_notifier.dart
+++ lib/auth/application/auth_notifier.dart
@@ -1,5 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:repo_viewer/auth/domain/auth_failure.dart';
+import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
 
 part 'auth_notifier.freezed.dart';
 
@@ -10,4 +12,39 @@ abstract class AuthState with _$AuthState {
   const factory AuthState.authenticated() = _Authenticated;
   const factory AuthState.unAuthenticated() = _Unauthenticated;
   const factory AuthState.failure(AuthFailure failure) = _Failure;
+}
+
+typedef AuthUriCallback = Future<Uri> Function(Uri authorizationUrl);
+
+class AuthNotifier extends StateNotifier<AuthState> {
+  AuthNotifier(this._authenticator) : super(const AuthState.initial());
+  final GithubAuthenticator _authenticator;
+
+  Future<void> checkAndUpdateAuthStatus() async {
+    state = (await _authenticator.isSignedIn())
+        ? const AuthState.authenticated()
+        : const AuthState.unAuthenticated();
+  }
+
+  Future<void> signIn(AuthUriCallback authorizationCallback) async {
+    final grant = _authenticator.createGrant();
+
+    final redirectUrl =
+        await authorizationCallback(_authenticator.getAuthorizationUrl(grant));
+    final successOrFailure = await _authenticator.handleAuthorizationResponse(
+        grant, redirectUrl.queryParameters);
+    state = successOrFailure.fold(
+      (failure) => AuthState.failure(failure),
+      (success) => const AuthState.authenticated(),
+    );
+    grant.close();
+  }
+
+  Future<void> signOut() async {
+    final failureOrSuccess = await _authenticator.signOut();
+    failureOrSuccess.fold(
+      (l) => AuthState.failure(l),
+      (r) => AuthState.unAuthenticated,
+    );
+  }
 }
