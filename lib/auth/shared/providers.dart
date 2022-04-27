0c2ecca9d4cc88de34a11fbd052f165d33559dbd
titis <titis.rizki@gmail.com>
2022-04-28 05:49:23 +0700

    prepare the provider (dependencies)

---
+++ lib/auth/shared/providers.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/application/auth_notifier.dart';
+import 'package:repo_viewer/auth/infrastructure/credentials_storage/credentials_storage.dart';
+import 'package:repo_viewer/auth/infrastructure/credentials_storage/secure_credentials_storage.dart';
+import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
+
+final fluterSecureStorage = Provider(
+  (ref) => const FlutterSecureStorage(),
+);
+final credentialsStorageProvider = Provider<CredentialsStorage>(
+  (ref) => SecureCredentialsStorage(ref.watch(fluterSecureStorage)),
+);
+
+final dioProvider = Provider(
+  (ref) => Dio(),
+);
+
+final gitthubAuthenticatorProvider = Provider(
+  (ref) => GithubAuthenticator(
+    ref.watch(credentialsStorageProvider),
+    ref.watch(dioProvider),
+  ),
+);
+
+final authNotifierProvider = StateNotifierProvider<AuthNotifier, AuthState>(
+  (ref) => AuthNotifier(
+    ref.watch(gitthubAuthenticatorProvider),
+  ),
+);
