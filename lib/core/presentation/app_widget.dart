76068d7533137d4bcda0dad7d8244461d7a950f7
titis <titis.rizki@gmail.com>
2022-04-28 12:10:16 +0700

    initialization provider

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/shared/providers.dart';
 import 'package:repo_viewer/core/presentation/routes/app_router.gr.dart';
 
-class AppWidget extends StatelessWidget {
+final initializationProvider = FutureProvider(
+  (ref) async {
+    final authNotifier = ref.read(authNotifierProvider.notifier);
+    await authNotifier.checkAndUpdateAuthStatus();
+  },
+);
+
+class AppWidget extends ConsumerWidget {
   AppWidget({Key? key}) : super(key: key);
   final appRouter = AppRouter();
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(initializationProvider, (previous, next) {});
     return MaterialApp.router(
       title: 'Repo Viewer',
       theme: ThemeData(
@@ -17,3 +27,25 @@ class AppWidget extends StatelessWidget {
     );
   }
 }
+
+
+// class AppWidget extends StatelessWidget {
+//   AppWidget({Key? key}) : super(key: key);
+//   final appRouter = AppRouter();
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return ProviderListener(
+//       provider: initializationProvider,
+//       onChange: (context, provider, child) {},
+//       child: MaterialApp.router(
+//         title: 'Repo Viewer',
+//         theme: ThemeData(
+//           primarySwatch: Colors.blue,
+//         ),
+//         routerDelegate: appRouter.delegate(),
+//         routeInformationParser: appRouter.defaultRouteParser(),
+//       ),
+//     );
+//   }
+// }
