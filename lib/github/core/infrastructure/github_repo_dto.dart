b7392457d09941fc37d3033b29eda45e79735357
titis <titis.rizki@gmail.com>
2022-05-08 18:53:48 +0700

    Github repo dto and user dto

---
+++ lib/github/core/infrastructure/github_repo_dto.dart
@@ -0,0 +1,43 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:repo_viewer/github/core/domain/github_repo.dart';
+import 'package:repo_viewer/github/core/infrastructure/user_dto.dart';
+
+part 'github_repo_dto.freezed.dart';
+part 'github_repo_dto.g.dart';
+
+String _descriptionFromJson(Object? json) {
+  return (json as String?) ?? '';
+}
+
+@freezed
+abstract class GithubRepoDto with _$GithubRepoDto {
+  const GithubRepoDto._();
+
+  const factory GithubRepoDto({
+    required UserDto owner,
+    required String name,
+    @JsonKey(fromJson: _descriptionFromJson) required String description,
+    @JsonKey(name: "stargazers_count") required int stargazersCount,
+  }) = _GithubRepoDto;
+
+  factory GithubRepoDto.fromJson(Map<String, dynamic> json) =>
+      _$GithubRepoDtoFromJson(json);
+
+  factory GithubRepoDto.fromDomain(GithubRepo _) {
+    return GithubRepoDto(
+      owner: UserDto.fromDomain(_.owner),
+      name: _.name,
+      description: _.description,
+      stargazersCount: _.stargazersCount,
+    );
+  }
+
+  GithubRepo toDomain() {
+    return GithubRepo(
+      owner: owner.toDomain(),
+      name: name,
+      description: description,
+      stargazersCount: stargazersCount,
+    );
+  }
+}
---
+++ lib/github/core/infrastructure/user_dto.dart
@@ -0,0 +1,31 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:repo_viewer/github/core/domain/user.dart';
+part 'user_dto.freezed.dart';
+part 'user_dto.g.dart';
+
+@freezed
+abstract class UserDto with _$UserDto {
+  const UserDto._();
+  const factory UserDto({
+    @JsonKey(name: 'login') required String name,
+    @JsonKey(name: 'avatar_url') required String avatarUrl,
+  }) = _UserDto;
+
+  factory UserDto.fromJsonManual(Map<String, dynamic> json) {
+    return UserDto(
+      name: json['login'] as String,
+      avatarUrl: json['avatar_url'] as String,
+    );
+  }
+
+  factory UserDto.fromJson(Map<String, dynamic> json) =>
+      _$UserDtoFromJson(json);
+
+  factory UserDto.fromDomain(User _) {
+    return UserDto(name: _.name, avatarUrl: _.avatarUrl);
+  }
+
+  User toDomain() {
+    return User(name: name, avatarUrl: avatarUrl);
+  }
+}
