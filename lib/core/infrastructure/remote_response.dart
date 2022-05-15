c69eb3bc74e5581febf9fb2d691fc7a965d4bb62
titis <titis.rizki@gmail.com>
2022-05-15 07:28:30 +0700

    modify remote response

--- lib/core/infrastructure/remote_response.dart
+++ lib/core/infrastructure/remote_response.dart
@@ -3,7 +3,10 @@ part 'remote_response.freezed.dart';
 
 @freezed
 abstract class RemoteResponse<T> with _$RemoteResponse<T> {
-  const factory RemoteResponse.noConnection() = _NoConnection<T>;
-  const factory RemoteResponse.notModified() = _NotModified<T>;
-  const factory RemoteResponse.withNewData(T data) = _WithNewData<T>;
+  const factory RemoteResponse.noConnection({required int maxPage}) =
+      _NoConnection<T>;
+  const factory RemoteResponse.notModified({required int maxPage}) =
+      _NotModified<T>;
+  const factory RemoteResponse.withNewData(T data, {required int maxPage}) =
+      _WithNewData<T>;
 }
--- lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -41,7 +41,8 @@ class StarredReposRemoteService {
         ),
       );
       if (response.statusCode == 304) {
-        return const RemoteResponse.notModified();
+        return RemoteResponse.notModified(
+            maxPage: previousHeaders?.link?.maxPage ?? 0);
       } else if (response.statusCode == 200) {
         final headers = GithubHeaders.parse(response);
         await _githubHeadersCache.saveHeaders(requestUrl, headers);
@@ -50,13 +51,15 @@ class StarredReposRemoteService {
             .map((e) => GithubRepoDto.fromJson(e))
             .toList();
 
-        return RemoteResponse.withNewData(convertedData);
+        return RemoteResponse.withNewData(convertedData,
+            maxPage: headers.link?.maxPage ?? 1);
       } else {
         throw RestApiException(response.statusCode);
       }
     } on DioError catch (e) {
       if (e.isNoConnectionError) {
-        return const RemoteResponse.noConnection();
+        return RemoteResponse.noConnection(
+            maxPage: previousHeaders?.link?.maxPage ?? 0);
       } else if (e.response != null) {
         throw RestApiException(e.response?.statusCode);
       } else {
